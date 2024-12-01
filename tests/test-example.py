26b850bce0c463d1330be1d53916a8b7ddc40d0e
caidensanders-wsu <caiden.sanders@wsu.edu>
2024-11-30 17:19:36 -0800

    Create test-example.py

---
+++ tests/test-example.py
@@ -0,0 +1,14 @@
+def add(a, b):
+  return a + b
+
+def test_add_1():
+  assert add(1, 2) == 3
+
+def test_add_2():
+  assert add(1, -2) == -1
+
+def test_add_3():
+  assert add(-1, -1) == -2
+
+def test_add_4():
+  assert add(-1, 8) == 7
